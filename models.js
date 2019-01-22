51bb4a12767e669e202ae32e9b79dd33783944a4
Sam Bengtson <sambeng1@gmail.com>
2019-01-22 16:43:16 -0600

    second commit

--- models.js
+++ models.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
-
 const Gardener = db.define('gardener', {
   name: Sequelize.STRING,
-  age: Sequelize.INTEGER
+  age: Sequelize.INTEGER,
+  favorite_veggie: { type: Sequelize.INTEGER, allowNull: false }
 });
 
 const Plot = db.define('plot', {
@@ -18,7 +18,11 @@ const Vegetable = db.define('vegetable', {
   planted_on: Sequelize.DATE
 });
 
+Plot.belongsTo(Gardener);
 
+Vegetable.belongsToMany(Plot, { through: 'veggie_plot' });
+Plot.belongsToMany(Vegetable, { through: 'veggie_plot' });
 
+Gardener.belongsTo(Vegetable, { as: 'favorite_veggie' });
 
-module.exports = { db };
+module.exports = { db, Gardener, Plot, Vegetable };
--- seed.js
+++ seed.js
@@ -1,12 +1,32 @@
-const { db } = require('./models');
+const { db, Gardener, Plot, Vegetable } = require('./models');
 
-db.sync({force: true})
-.then(() => {
-  console.log('it worked!');
-})
-.catch((err) => {
-  console.log('DISASTER!', err);
-})
-.finally(() => {
-  db.close();
-})
+const veggieData = [
+  { name: 'carrot', color: 'orange', planted_on: Date() },
+  { name: 'onion', color: 'yellow', planted_on: Date() }
+];
+const gardenerData = [{ name: 'Tom', age: 99 }, { name: 'Sam', age: 99 }];
+const plotData = [{ size: 5, shaded: true }, { size: 7, shaded: false }];
+
+db.sync({ force: true })
+  .then(() => {
+    console.log('it worked!');
+    const veggiePromise = Vegetable.bulkCreate(veggieData, { returning: true });
+    const gardenerPromise = Gardener.bulkCreate(gardenerData, {
+      returning: true
+    });
+    const plotPromise = Plot.bulkCreate(plotData, { returning: true });
+
+    return Promise.all([veggiePromise, gardenerPromise, plotPromise]);
+  })
+  // .then(seededStuff => {
+  //   const [vegetable, gardener, plot] = seededStuff;
+  //   const [name, color, planted_on] = vegetable;
+  //   const [name, age] = gardener;
+  //   const [size, shaded] = plot;
+  // })
+  .catch(err => {
+    console.log('DISASTER!', err);
+  })
+  .finally(() => {
+    db.close();
+  });
